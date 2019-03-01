788cfc13c3112d2377cbe3ba1595273dc57b23d7
34748 <347487522@qq.com>
2019-03-01 15:30:52 +0800

    安装环境_01

---
+++ app/main.js
@@ -0,0 +1,9 @@
+/**
+ * @author: LYLG_zkx
+ * @Email: 347487522@qq.com
+ * @date: 2019/3/1 15:17
+ * @IDE: WebStorm
+ */
+
+const greeter = require('./Greeter');
+document.querySelector('#root').appendChild(greeter());
